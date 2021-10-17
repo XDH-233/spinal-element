19ca465cbca36e79a50737fa2dcd5f3b64a0c6eb
Pu Wang <nicolas.weeks@gmail.com>
2021-10-17 23:47:04 +0800

    setup scala

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
+# Setup Scala CI
+# https://diamantidis.github.io/2020/05/17/ci-with-github-actions-for-scala-project
+
 set -o errexit
 set -o nounset
 set -o xtrace
 
-sbt about
+touch build.sbt
+touch .scalafmt.conf
+sbt about scalafmtCheckAll scalafmtSbtCheck 'scalafix --check'
+sbt clean coverage coverageReport test
+
